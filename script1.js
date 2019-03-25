e4bc2176d909e9097adad6cfd295f8f441b21ec8
MenkuiChan <ilonaspolding@gmail.com>
2019-03-25 21:30:40 +0300

    first try to do something with DOM and JS

---
+++ script1.js
@@ -0,0 +1,6 @@
+var newTask = document.getElementById("new-task");
+var incompleteTasks = document.getElementById("incomplete-tasks");
+var completedTasks = document.getElementById("completed-tasks");
+var addButton = document.getElementsByTagName("button")[0]; //First button
+
+//New Task List item
