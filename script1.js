745c039e612bb1aa137cc2a340c0da76ebf0c648
MenkuiChan <ilonaspolding@gmail.com>
2019-03-25 22:22:10 +0300

    add function, that creates list items

--- script1.js
+++ script1.js
@@ -1,6 +1,22 @@
 var newTask = document.getElementById("new-task");
 var incompleteTasks = document.getElementById("incomplete-tasks");
 var completedTasks = document.getElementById("completed-tasks");
-var addButton = document.getElementsByTagName("button")[0]; //First button
+var addButton = document.getElementsByTagName("button")[0]; // first button
 
-//New Task List item
+// new Task List item
+
+var createNewTaskElement = function (newTaskString) {
+    // create List Item
+    var listItem = document.createElement("li");
+    // input checkbox
+    var checkBox = document.createElement("input");
+    // label
+    var label = document.createElement("label");
+    // button.delete
+    var deleteButton = document.createElement("button");
+
+    // each element needs modified 
+    checkBox.type = "checkBox";
+    deleteButton.innerText = "Delete";
+    deleteButton.className = "delete";
+}
