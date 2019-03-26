7fa1719f256a5601e59add677d962822e583b174
MenkuiChan <ilonaspolding@gmail.com>
2019-03-26 17:09:59 +0300

    adding task is working

--- script1.js
+++ script1.js
@@ -1,4 +1,4 @@
-var newTask = document.getElementById("new-task");
+var taskInput = document.getElementById("new-task");
 var incompleteTasks = document.getElementById("incomplete-tasks");
 var completedTasks = document.getElementById("completed-tasks");
 var addButton = document.getElementsByTagName("button")[0]; // first button
